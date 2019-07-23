507a900163f0c8b4c6de25749fd36c17b0e443b5
Daniel Alm <Daniel.Alm@ForumD.net>
2019-07-23 10:35:21 +0200

    Initial Commit

---
+++ Swift5PerformanceTest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift5PerformanceTest
+//
+//  Created by Daniel Alm on 23.07.19.
+//  Copyright © 2019 Daniel Alm. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
